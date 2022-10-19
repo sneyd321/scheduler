d509f9f9c4acc0db29bebc26c3a6b59ab651b423
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-19 14:07:09 -0400

    change docker config

--- remove_key.py
+++ remove_key.py
@@ -3,5 +3,5 @@ import json, os
 with open("./ServiceAccount.json") as serviceAccount:
     key = json.load(serviceAccount)
 
-os.system(f"gcloud iam service-accounts keys delete {key['private_key_id']} --iam-account=firebase-adminsdk-rbs1y@roomr-222721.iam.gserviceaccount.com --quiet")  
+os.system(f"gcloud iam service-accounts keys delete {key['private_key_id']} --iam-account=scheduler@roomr-222721.iam.gserviceaccount.com --quiet")  
 
