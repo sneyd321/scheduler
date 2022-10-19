830eb4ba9e3f6a62c9e959742cbfed5a6b11c4a6
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-19 13:47:57 -0400

    change docker config

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ async def upload_maintenance_ticket(request: MaintenanceTicketUploadSchema):
     return {"status": "Job scheduled successfully"}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8084)))
+    uvicorn.run(app, port=int(os.environ.get("PORT", 8084)))
