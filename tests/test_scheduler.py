9d2db3f30ae0990a2f79b050f4cb1e77f1578ece
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-20 13:39:42 -0400

    debug credentials issue

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,12 +1,17 @@
 from models.scheduler import Scheduler
-import time
+import os
 
 
 
-scheduler = Scheduler()
+
 
 
 async def test_Scheduler_successfully_schedules_job():
+    stream = os.popen('/usr/lib/google-cloud-sdk/bin/gcloud auth list')
+    output = stream.read()
+    print(output)
+    print("GFDGADSFDSAF")
+    scheduler = Scheduler()
     uuidValue = scheduler.schedule_job("upload-service", {})
     check = False
     for item in scheduler.list_jobs():
@@ -18,5 +23,6 @@ async def test_Scheduler_successfully_schedules_job():
 
 
 async def test_Scheduler_returns_error_when_duplicate_id_is_created():
+    scheduler = Scheduler()
     uuidValue = scheduler.schedule_job("upload-service", {})
     assert scheduler.job_exists(uuidValue)
