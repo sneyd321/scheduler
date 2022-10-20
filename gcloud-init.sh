455088b4353f937ecf0bdb99b0d167f05da916b3
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-20 11:52:55 -0400

    update docker image to slim

--- gcloud-init.sh
+++ gcloud-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcloud auth activate-service-account scheduler@roomr-222721.iam.gserviceaccount.com --key-file=/usr/src/app/ServiceAccount.json
-gcloud container clusters get-credentials roomr-cluster-1 --project=roomr-222721 --region=us-central1
+gcloud container clusters get-credentials roomr-cluster-1 --project=roomr-222721 --region=us-central1 --zone=us-central1-a
