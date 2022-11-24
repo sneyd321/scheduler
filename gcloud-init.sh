7422961999c3bfa3d5911683672f89d7264d33ab
Ryan Sneyd <rts1234567@hotmail.com>
2022-11-24 10:18:07 -0500

    chnage region

--- gcloud-init.sh
+++ gcloud-init.sh
@@ -2,6 +2,6 @@
 
 gcloud auth activate-service-account scheduler@roomr-222721.iam.gserviceaccount.com --key-file=/usr/src/app/ServiceAccount.json
 export ENV GOOGLE_APPLICATION_CREDENTIALS=/usr/src/app/ServiceAccount.json
-gcloud container clusters get-credentials roomr-cluster-1 --project=roomr-222721 --region=us-central1 
+gcloud container clusters get-credentials  autopilot-cluster-1 --project=roomr-222721 --region=us-east5 
 
 cp ~/.kube/config /usr/src/app/config 
